5112946e0169263a930bbd59ef91d32603338cdf
EngineerHamziey <engineerlawalhamzatademola@gmail.com>
2023-03-11 10:49:11 -0800

    project setup done, now starting to build

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import User from './User'
+
+const Card = () => {
+  return (
+    <div>
+      <User />
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/MyFrontendMentorFooter.jsx
@@ -0,0 +1,21 @@
+
+
+const MyFrontendMentorFooter = () => {
+  return (
+    <footer class="attribution">
+      <p class="attribution__p"> 
+          Coded by 
+        <a class="btn attribution__link" href="https://www.linkedin.com/in/hamzat-lawal-a88404239">
+          Lawal Hamzat
+        </a>.
+        <a class="btn attribution__link" href="https://github.com/EngineerHamziey/testimonials-grid-section-main-react-app">Code Link.</a>
+        Challenge by
+        <a class="btn attribution__link" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+          Frontend Mentor.
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default MyFrontendMentorFooter
